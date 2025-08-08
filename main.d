f7567dd69744c2dfde33d0d0c3103420ca131d0f
whiteapolo <darkpendraw@proton.me>
2025-08-08 14:10:51 +0300

    d

--- main.d
+++ main.d
@@ -4,70 +4,90 @@ import std.conv;
 import std.string;
 import std.algorithm;
 import std.exception;
+import std.getopt;
 
-const float MIN_BRIGHTNESS = 0.001;
-const float MAX_BRIGHTNESS = 1.0;
+const float MIN_BRIGHTNESS = 0.1;
+const float MAX_BRIGHTNESS = 100.0;
 
 const string[] POSSIBLE_DEVICES = [
-  "/sys/class/backlight/intel_backlight",
-  "/sys/class/backlight/acpi_video0"
+    "/sys/class/backlight/intel_backlight", "/sys/class/backlight/acpi_video0"
 ];
 
-string detectDevicePath() {
+string detectDevicePath()
+{
 
-  foreach (devicePath; POSSIBLE_DEVICES) {
-    if (devicePath.exists) {
-      return devicePath;
+    foreach (devicePath; POSSIBLE_DEVICES)
+    {
+        if (devicePath.exists)
+        {
+            return devicePath;
+        }
     }
-  }
 
-  throw new Exception("device name not found.");
+    throw new Exception("device name not found.");
 }
 
-string getDevicePath() {
-  static string devicePath = null;
-  return devicePath ? devicePath : (devicePath = detectDevicePath());
+string getDevicePath()
+{
+    static string devicePath = null;
+    return devicePath ? devicePath : (devicePath = detectDevicePath());
 }
 
-float getBrightness() {
-  int maxBrightness = to!int(readText(getDevicePath() ~ "/max_brightness").strip());
-  int brightness = to!int(readText(getDevicePath() ~ "/brightness").strip());
+float getBrightness()
+{
+    int maxBrightness = to!int(readText(getDevicePath() ~ "/max_brightness").strip());
+    int brightness = to!int(readText(getDevicePath() ~ "/brightness").strip());
 
-  return cast(float)brightness / maxBrightness;
+    return (cast(float) brightness / maxBrightness) * 100;
 }
 
-void setBrightness(float value) {
-  int maxBrightness = to!int(readText(getDevicePath() ~ "/max_brightness").strip());
-  int newBrightness = cast(int)(maxBrightness * clamp(value, MIN_BRIGHTNESS, MAX_BRIGHTNESS));
+void setBrightness(float value)
+{
+    int maxBrightness = to!int(readText(getDevicePath() ~ "/max_brightness").strip());
+    int newBrightness = cast(int)(maxBrightness * clamp(value, MIN_BRIGHTNESS,
+            MAX_BRIGHTNESS) / 100.0);
 
-  std.file.write(getDevicePath() ~ "/brightness", to!string(newBrightness));
+    std.file.write(getDevicePath() ~ "/brightness", to!string(newBrightness));
 }
 
-int main(string[] args) {
-
-  if (args.length == 1) {
-    writeln(getBrightness());
-    return 0;
-  }
-
-  enforce(args.length == 3, "Usage: [up|down|set] <percent>");
-
-  auto operator = args[1];
-  auto value = args[2];
-
-  enforce(value.isNumeric(), "Argument must be a number");
-
-  final switch (args[1]) {
+void argumentHandler(string option, string value)
+{
+    final switch (option)
+    {
     case "up":
-      setBrightness(getBrightness() + (to!float(args[2]) / 100.0));
-      break;
+        setBrightness(getBrightness() + (to!float(value)));
+        break;
     case "down":
-      setBrightness(getBrightness() - (to!float(args[2]) / 100.0));
-      break;
+        setBrightness(getBrightness() - (to!float(value)));
+        break;
     case "set":
-      setBrightness(to!float(args[2]) / 100.0);
-      break;
-  }
+        setBrightness(to!float(value));
+        break;
+    }
+}
 
-  return 0;
+int main(string[] args)
+{
+    if (args.length == 1)
+    {
+        writeln(getBrightness(), "%");
+        return 0;
+    }
+
+    try
+    {
+        auto opt = getopt(args, "set", &argumentHandler, "up",
+                &argumentHandler, "down", &argumentHandler);
+
+        if (opt.helpWanted)
+        {
+            defaultGetoptPrinter("set screen backlight.", opt.options);
+        }
+    }
+    catch (Exception e)
+    {
+        writeln(e.msg);
+    }
+
+    return 0;
 }
